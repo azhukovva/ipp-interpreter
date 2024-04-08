8d43d915e88c66c8901e4618e4a9712eb8f5fa07
azhukovva <zhukovaarina509@gmail.com>
2024-04-08 17:53:14 +0200

    fix: Removed spaces and newlines from XML DOM

--- student/Interpreter.php
+++ student/Interpreter.php
@@ -14,7 +14,27 @@ class Interpreter extends AbstractInterpreter
         // TODO: Start your code here
 
         // Check \IPP\Core\AbstractInterpreter for predefined I/O objects:
+
         $dom = $this->source->getDOMDocument();
+
+        /**
+         * REVIEW
+         * preserveWhiteSpace is set to true by default. This means that any spaces, tabs, newlines, or other whitespace characters 
+         * found within elements and between elements will be preserved as part of the text nodes
+         * 
+         * If set to false, the parser does not preserve white space, It trims leading and trailing white space 
+         * from text nodes
+         * 
+         * <3
+         */
+        $dom->preserveWhiteSpace = false;
+
+        /**
+         * REVIEW
+         * After preserveWhiteSpace was set to false, we need to rebuild  xml again
+         */
+        $dom->loadXml($dom->saveXML());
+
         // $val = $this->input->readString();
         // $this->stdout->writeString("stdout");
         // $this->stderr->writeString("stderr");
@@ -22,11 +42,11 @@ class Interpreter extends AbstractInterpreter
         // handle options input
         // parse the XML document
         // INSTRUCTIONS's array
-        $instructions = ParserXML::parseXML($dom);
+        $instructions = ParserXML::parseXML($dom); 
+        
 
-
+        print_r($instructions);
 
         return 0;
-        // throw new NotImplementedException;
     }
 }
